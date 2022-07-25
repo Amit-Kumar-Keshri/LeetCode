ae3f332a4a4136a331efebe069c1af352a1e86bd
Amit-Kumar-Keshri <73033545+Amit-Kumar-Keshri@users.noreply.github.com>
2022-07-25 21:26:46 +0530

    Time: 85 ms (5.10%), Space: 14 MB (13.75%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -6,18 +6,13 @@ class Solution:
             res += int(i)**2
         return res
     def isHappy(self, n: int) -> bool:
-        t=n
-        c = 0
-        while t>0:
-            c+=1
-            t//=10
-        if c==1:
-            c = n
-        else:
-            c = c**2+1
-        while c>0:
-            n=self.s(n)
+        repeat = set()
+        while True:
+            repeat.add(n)
+            n = self.s(n)
             if n==1:
                 return True
-            c-=1
-        return False
+            elif n in repeat:
+                return False
+            
+            

6def95cc69a7c03d2b78a184b78134f76d2a0712
Amit-Kumar-Keshri <73033545+Amit-Kumar-Keshri@users.noreply.github.com>
2022-07-25 18:43:07 +0530

    Time: 200 ms (5.07%), Space: 15.5 MB (50.23%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,42 @@
+class Solution:
+    def firstoccr(self, nums,target):
+        first = -1
+        l = 0
+        h = len(nums)-1
+        if len(nums)==0:
+            return -1
+        while l<=h:
+            m = (l+h)//2
+            temp = nums[m]
+            if temp==target:
+                first = m
+                h = m-1
+            elif target > temp:
+                l = m+1
+            else:
+                h = m-1
+        return first
+    def lastoccr(self, nums,target):
+        last = -1
+        l = 0
+        h = len(nums)-1
+        if len(nums)==0:
+            return -1
+        while l<=h:
+            m = (l+h)//2
+            temp = nums[m]
+            if temp==target:
+                last = m
+                l = m+1
+            elif target > temp:
+                l = m+1
+            else:
+                h = m-1
+        
+        return last
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        ans = []
+        ans.append(self.firstoccr(nums,target))
+        ans.append(self.lastoccr(nums,target))
+        return ans
+                    

b7a48b298f587235620b7f065911ffd3997a7f27
Amit-Kumar-Keshri <73033545+Amit-Kumar-Keshri@users.noreply.github.com>
2022-07-26 23:27:07 +0530

    Time: 73 ms (56.26%), Space: 14.4 MB (66.69%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,3 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return int(Counter(s)==Counter(t))
+        l1=[0]*26
+
+        # for i in range(26):
+        #     l1.append(0)
+
+        for i in s:
+            l1[ord(i)-97] += 1
+            
+        for i in t:
+            l1[ord(i)-97] -= 1   
+            
+        if l1.count(0)==26:
+             return 1
+        return 0

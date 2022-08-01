de94f3a35c251645d246f1265367456fdf0c9ff6
Amit-Kumar-Keshri <73033545+Amit-Kumar-Keshri@users.noreply.github.com>
2022-08-01 22:35:42 +0530

    Time: 254 ms (57.91%), Space: 15.3 MB (49.49%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.py
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.py
@@ -10,17 +10,24 @@ class Solution:
         
         while curr < n:
             start = curr
-            if start+1 <n and arr[start] < arr[start+1]:
-                while start+1 <n and arr[start] < arr[start+1]:
-                    start += 1
-                    
-                if start+1 <n and arr[start] > arr[start+1]:
-                    while start+1 <n and arr[start] > arr[start+1]:
-                        start += 1
-                        
-                    lenn = start - curr +1
-                    if max_l < lenn:
-                        max_l = lenn
+            lenn = 0
+            flag = False
+            flag1 = False
+            # if start+1 <n and arr[start] < arr[start+1]:
+            while start+1 <n and arr[start] < arr[start+1]:
+                start += 1
+                lenn +=1
+                flag = True
+            # if start+1 <n and arr[start] > arr[start+1]:
+            while start+1 < n and arr[start] > arr[start+1]:
+                start += 1
+                lenn +=1
+                flag1 = True
+            if lenn != 0:
+                lenn+=1
+            
+            if flag and flag1 and max_l < lenn:
+                max_l = lenn
             if max_l > n//2:
                 return max_l
             

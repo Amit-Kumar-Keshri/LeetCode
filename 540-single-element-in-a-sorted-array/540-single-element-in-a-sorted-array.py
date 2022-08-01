ccf95b6e156e9d65554a610e43025708b9ff8dfb
Amit-Kumar-Keshri <73033545+Amit-Kumar-Keshri@users.noreply.github.com>
2022-08-02 02:58:38 +0530

    Time: 279 ms (46.91%), Space: 23.7 MB (46.12%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -3,25 +3,29 @@ class Solution:
         n = len(arr)
         l = 0
         h = n-1
-        if n==1:
-            return arr[0]
+        # if n==1:
+        #     return arr[0]
         
         while l<=h:
             m = (l+h)//2
+            if (m == 0 or arr[m-1] != arr[m]) and (m == n-1 or arr[m+1] != arr[m]):
+                return arr[m]
             
-            if m%2==0:
+            elif m%2==0:
                 if arr[m] == arr[m-1]:
                     h = m-1
                 elif m+1<n and arr[m] == arr[m+1]:
                     l = m+1
-                else:
-                    return arr[m]
+                # else:
+                #     return arr[m]
+            elif m%2==1:
+                if arr[m] == arr[m-1]:
+                    l = m+1
+                elif m+1<n and arr[m] == arr[m+1]:
+                    h = m-1
+                # else:
+                #     return arr[m]
             else:
-                if arr[m] == arr[m-1]:
-                    l = m+1
-                elif m+1<n and arr[m] == arr[m+1]:
-                    h = m-1
-                else:
-                    return arr[m]
+                return arr[0]
                 
-                
+    

f2289652e3403a781ac40acedfdf2cf737837a40
Amit-Kumar-Keshri <73033545+Amit-Kumar-Keshri@users.noreply.github.com>
2022-08-02 05:56:47 +0530

    Time: 40 ms (71.90%), Space: 13.8 MB (57.96%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.py
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.py
@@ -3,7 +3,6 @@ class Solution:
         s = ""
         while columnNumber != 0:
             columnNumber -=1
-            x = columnNumber%26
-            s = chr(x+65) +s
-            columnNumber = columnNumber //26
+            s = chr(columnNumber%26 + 65) +s
+            columnNumber = columnNumber // 26
         return s

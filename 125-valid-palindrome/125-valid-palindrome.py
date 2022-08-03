5e7bbc3aaf23831b7018327a77016689a8dff7fb
Amit-Kumar-Keshri <73033545+Amit-Kumar-Keshri@users.noreply.github.com>
2022-08-04 03:02:23 +0530

    Time: 114 ms (13.17%), Space: 14.6 MB (42.08%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs = ""
+        
+        for i in s:
+            if i.islower():
+                strs+=i
+            elif i.isupper():
+                strs+=i.lower()
+            elif i.isnumeric():
+                strs+=i
+            else:
+                continue
+        print(strs)
+        l = 0
+        h = len(strs)-1
+        while l<=h:
+            if strs[l]!=strs[h]:
+                return False
+            else:
+                l += 1
+                h -= 1
+            
+        return True
+            

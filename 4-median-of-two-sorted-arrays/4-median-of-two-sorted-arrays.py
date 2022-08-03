d0451ecbd7b959d04199aa785af9aae793811bc3
Amit-Kumar-Keshri <73033545+Amit-Kumar-Keshri@users.noreply.github.com>
2022-08-04 00:27:52 +0530

    Time: 169 ms (33.81%), Space: 14.3 MB (25.87%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,25 +1,26 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        mg = []
-        
-        
-        i = 0
-        j = 0
-        
-        while i<len(nums1) and j<len(nums2):
-            
-            if nums1[i] <= nums2[j]:
-                mg.append(nums1[i])
-                i += 1
-            else:
-                mg.append(nums2[j])
-                j += 1
-        for i in range(i,len(nums1)):
-            mg.append(nums1[i])
-        for i in range(j,len(nums2)):
-            mg.append(nums2[i])
-        
-        if len(mg)%2==1:
-            return mg[len(mg)//2]
-        else:
-            return (mg[len(mg)//2]+mg[(len(mg)//2)-1])/2
+        n=len(nums1)
+        m=len(nums2)
+        if m<n:
+            return self.findMedianSortedArrays(nums2,nums1)
+        start=0
+        end=n
+        while start<=end:
+            i1=int((start+end)/2)
+            i2=int((m+n+1)/2)-i1
+            min1=1000000 if i1==n else nums1[i1]
+            min2=1000000 if i2== m else nums2[i2]
+            max1=-1000000if i1==0 else nums1[i1-1]
+            max2=-1000000 if i2==0 else nums2[i2-1]
+
+            if max1<=min2 and max2<=min1:
+                if (m+n)%2==0:
+                    return (max(max1,max2)+min(min1,min2))/2
+                else:
+                    return max(max1,max2)
+            elif max1>min2:
+                end=i1-1
+            elif max2>min1:
+                start=i1+1
+        return 0 

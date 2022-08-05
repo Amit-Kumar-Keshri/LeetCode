8d70599b4ac1214f9b3cf44a1a9e78cb7bb1406c
Amit-Kumar-Keshri <73033545+Amit-Kumar-Keshri@users.noreply.github.com>
2022-08-06 04:06:07 +0530

    Time: 164 ms (50.30%), Space: 17.8 MB (67.02%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, nums: List[str]) -> List[List[str]]:
+        ans = []
+        m = {}
+        
+        n = len(nums)
+        for i in nums:
+            x = sorted(i)
+            y = "".join(x)
+            
+            if y not in m:
+                m[y] = [i]
+                
+            else:
+                 m[y].append(i)
+                
+        return m.values()

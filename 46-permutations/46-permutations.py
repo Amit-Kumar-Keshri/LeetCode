1a21557ae722365f6b980e43876acf089aa6bd5f
Amit-Kumar-Keshri <73033545+Amit-Kumar-Keshri@users.noreply.github.com>
2022-08-12 04:23:08 +0530

    Time: 38 ms (97.23%), Space: 14 MB (84.46%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,42 @@
+from copy import deepcopy
+class Solution:
+    def fact(self, n: int) -> int:
+        if n==0:
+            return 1
+        return n*self.fact(n-1)
+    
+    def nxtper(slef, nums):
+
+        i = j = len(nums)-1
+        while i > 0 and nums[i-1] > nums[i]:
+            i -=1
+
+
+        while j > 0 and nums[i-1] > nums[j]:
+            j-=1
+        nums[i-1], nums[j] = nums[j], nums[i-1]
+
+        left = i
+        right = len(nums) - 1
+        while left <= right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+        return nums
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        # x = 1
+        # for i in range(1,n+1):
+        #     x *= i
+        x = self.fact(n)
+        ans = []
+        
+        i = 0
+        while i < x: 
+            self.nxtper(nums)
+            ans.append(deepcopy(nums))
+            i+= 1
+            
+
+        return ans
